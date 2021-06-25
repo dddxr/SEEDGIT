48940c3de39a91973d28679f08243b23917d4aca
admin <admin@DESKTOP-8CU4IDJ>
2021-06-25 11:20:36 +0900

    jun commit

---
+++ GitEx/src/main/java/com/seed/gitEx/PracticeJun.java
@@ -0,0 +1,10 @@
+package com.seed.gitEx;
+
+public class PracticeJun {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Practice Jun");
+		}
+	}
+}
